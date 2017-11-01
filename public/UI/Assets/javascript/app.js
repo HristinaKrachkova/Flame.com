73a4acf20a4c676d0505211a7fafadee852bfb5c
Hristo Sabev <hristo.sabev@Hristos-MacBook-Pro.local>
2017-11-01 18:51:33 +0200

    Some changes

--- public/UI/Assets/javascript/app.js
+++ public/UI/Assets/javascript/app.js
@@ -2,17 +2,17 @@
 var app = angular.module('myApp', ["ngRoute"]);
 app.controller('registrationForm', function($scope) {
     $scope.registerAUser = function() {
-        // var password = $("#password").val();
-        // var name = $("#name").val();
-        // var lastName = $("#lastName").val();
-        // var email = $("#email").val();
-        // var finder = userDB.users.find(a=>a.email == email)
-        // if(!finder){
-        //     userDB.addUser(name, lastName, password, email);
-        //     console.log(userDB);
-        //     alert("You are registered now!")
-        // }else{
-        //     alert("We already have this user!")
-        // }
+        var password = $("#password").val();
+        var name = $("#name").val();
+        var lastName = $("#lastName").val();
+        var email = $("#email").val();
+        var finder = userDB.users.find(a=>a.email == email)
+        if(!finder){
+            userDB.addUser(name, lastName, password, email);
+            console.log(userDB);
+            alert("You are registered now!")
+        }else{
+            alert("We already have this user!")
+        }
     }
 });
