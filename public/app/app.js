28b43422ce4b18c8d0a5602858d1872987275261
HristinaKrachkova <hristina,krachkova@gmail.com>
2017-11-02 09:32:05 +0200

    .

--- public/app/app.js
+++
@@ -1,5 +0,0 @@
-angular.module('userApp', ['appRoutes', 'userControllers', 'userServices', 'ngAnimate', 'mainController', 'authServices', 'emailController', 'managementController'])
-
-.config(function($httpProvider) {
-    $httpProvider.interceptors.push('AuthInterceptors');
-});
