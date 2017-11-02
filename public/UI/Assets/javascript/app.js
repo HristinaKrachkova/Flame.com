b6ef16e76f28e3c6e7e90a8202e2e86303c411b3
Ralitsa Solakova <rsolakova@yahoo.com>
2017-11-02 22:34:57 +0200

    Add html newPeople - page for matching; Add buttons - Like and Dislike;

--- public/UI/Assets/javascript/app.js
+++ public/UI/Assets/javascript/app.js
@@ -1,64 +1,74 @@
 
-var app = angular.module('myApp', ["ngRoute"]);
+var app = angular.module('myApp', ['ngRoute']);
 
 app.config(function($routeProvider) {
     $routeProvider
-    .when("/", {
-        templateUrl : "mainPage.html"
-    })
-    .when("/login",{
-        templateUrl : "emailLogIn.html"
-    })
-    .when("/user",{
-        templateUrl : "userProfile.html",
-    })
-    .when("/messages",{
-        templateUrl : "userMessages.html"
-    })
+        .when('/', {
+            templateUrl: 'mainPage.html'
+        })
+        .when('/login', {
+            templateUrl: 'emailLogIn.html'
+        })
+        .when('/user', {
+            templateUrl: 'userProfile.html'
+        })
+        .when('/messages', {
+            templateUrl: 'userMessages.html'
+        })
+        .when('/newpeople', {
+            templateUrl: 'newPeople.html'
+        });
 })
-.controller('registrationForm', function($scope, $location) {
-    $scope.registerAUser = function () {
-        event.preventDefault();
-        var password = $("#password").val();
-        var name = $("#name").val();
-        var lastName = $("#lastName").val();
-        var email = $("#email").val();
+    .controller('registrationForm', function($scope, $location) {
+        $scope.registerAUser = function () {
+            event.preventDefault();
+            var password = $('#password').val();
+            var name = $('#name').val();
+            var lastName = $('#lastName').val();
+            var email = $('#email').val();
 
-        userDB.register(name, lastName, password, email, function (data) {
-            if (data.success) {
-                alert("You are registered now!");
-            } else {
-                console.log(data.error);
-                alert("There was a problem with your registration");
-            }
-        });
-    }
-})
-.controller('login', function($scope, $location){
-    $scope.login = function(){
-        event.preventDefault();
-        var logInPass = $("#logInPass").val();
-        var logInEmail = $("#logInEmail").val();
-        
-        userDB.login(logInEmail, logInPass, function (data) {
-            if (data.success == true) {
-                $("#profileLink").removeClass("disabled");
-                $("#findLink").removeClass("disabled");
-                $("#messagesLink").removeClass("disabled");
-                $location.path('/user');
-                $scope.$apply();
-            } else {
-                console.log(data);
-                alert("Nope!");
-            }
-        });
-    }
-})
-.controller("profile", function($scope){
-    $scope.signedUser = userDB.signedUser;
-    $scope.saveChanges = function(){
-        userDB.signedUser.setUserAge($("#input1").val());
-        userData();
-        
-    }
-});
+            userDB.register(name, lastName, password, email, function (data) {
+                if (data.success) {
+                    alert('You are registered now!');
+                } else {
+                    console.log(data.error);
+                    alert('There was a problem with your registration');
+                }
+            });
+        };
+    })
+    .controller('login', function($scope, $location) {
+        $scope.login = function() {
+            event.preventDefault();
+            var logInPass = $('#logInPass').val();
+            var logInEmail = $('#logInEmail').val();
+
+            userDB.login(logInEmail, logInPass, function (data) {
+                if (data.success == true) {
+                    $('#profileLink').removeClass('disabled');
+                    $('#findLink').removeClass('disabled');
+                    $('#messagesLink').removeClass('disabled');
+                    $location.path('/user');
+                    $scope.$apply();
+                } else {
+                    console.log(data);
+                    alert('Nope!');
+                }
+            });
+        };
+    })
+    .controller('profile', function($scope) {
+        $scope.signedUser = userDB.signedUser;
+        $scope.saveChanges = function() {
+            userDB.signedUser.setUserAge($('#input1').val());
+            userData();
+        };
+    })
+    .controller('newpeople', function($scope) {
+        $scope.likeUser = function () {
+            alert('I like this');
+        };
+        $scope.dislikeUser = function () {
+            alert('I don`t like this');
+        };
+    });
