acf7b1c758900eb40ee0ea243f55cc3e12eea9a9
HristinaKrachkova <hristina,krachkova@gmail.com>
2017-11-07 19:21:46 +0200

    heroku test

--- public/UI/Assets/javascript/app.js
+++ public/UI/Assets/javascript/app.js
@@ -43,7 +43,7 @@ app.config(function($routeProvider) {
             });
     })
     .factory('socket', function($rootScope) {
-        var socket = io.connect('http://127.0.0.1:4000');
+        var socket = io.connect('https://young-crag-36367.herokuapp.com');
 
         return {
             on: function(eventName, callback) {
